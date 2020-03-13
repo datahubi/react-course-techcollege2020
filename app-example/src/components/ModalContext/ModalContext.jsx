fba6eb5bb6673f537345d72ca19915d43ad05a8b
mftw <michaelnlkoch@gmail.com>
2020-03-13 17:12:01 +0100

    modal and scrollprogress

---
+++ app-example/src/components/ModalContext/ModalContext.jsx
@@ -0,0 +1,53 @@
+import React, { createContext, useState } from 'react'
+import styles from './ModalContext.module.scss';
+
+
+const Modal = props => {
+  // const context = useContext(ModalContext)
+  const { modalContent, hideModal } = props;
+  return (
+    <div className={styles.wrapper} onClick={hideModal} >
+      <div className={styles.container}>
+        {modalContent || "Dette er en modal "}
+        <button onClick={hideModal}>close</button>
+      </div>
+    </div>
+  )
+}
+
+// Dette er udelukkende til intellisense i de filer
+// dette importeres i, så editoren kan foreslå ting.
+// Objektet indeholder bare no-op funktioner og tomme
+// strings.
+const modalContext = {
+  showModal: x => x,
+  hideModal: x => x,
+  toggleModal: x => x,
+  setModalContent: x => x,
+  clearModalContent: x => x,
+  modalContent: "",
+}
+
+export const ModalContext = createContext(modalContext);
+
+
+export default function ModalProvider(props) {
+  const [showModal, setShowModal] = useState(false);
+  const [modalContent, setModalContent] = useState(null)
+
+  const context = {
+    showModal: () => setShowModal(true),
+    hideModal: () => setShowModal(false),
+    toggleModal: () => setShowModal(!showModal),
+    setModalContent: setModalContent,
+    clearModalContent: () => setModalContent(""),
+    modalContent: modalContent,
+  }
+
+  return (
+    <ModalContext.Provider value={context}>
+      {props.children}
+      {showModal && <Modal {...context} />}
+    </ModalContext.Provider>
+  )
+}
--- app-example/src/components/Pages/Contact/Contact.jsx
+++ app-example/src/components/Pages/Contact/Contact.jsx
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { useContext } from "react";
+import { ModalContext } from "../../ModalContext/ModalContext";
 
-const ContactPage = props => (
+const ContactPage = props => {
+  const { showModal, setModalContent } = useContext(ModalContext);
+
+  const showContactModal = () => {
+    setModalContent(
+      <>
+        <h3>Contact Info</h3>
+        <ul>
+          <li>Tlf: xxxxxxxx</li>
+          <li>Adresse: Andebyvej 1337</li>
+          <li>By: Andeby</li>
+          <li>Navn: Anders And</li>
+        </ul>
+      </>
+    )
+    showModal();
+  }
+  return (
   <>
     <h2>Contact</h2>
     <p>Kontakt os nu bare!</p>
+    <button onClick={showContactModal}>Vis kontakt info</button>
   </>
-);
+)};
 
 export default ContactPage;
